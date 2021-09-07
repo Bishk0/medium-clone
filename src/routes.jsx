5dc6ba08af784a92298694cd9851272c04e9bdf2
Yurii Bishko <yurii.bishko@indeema.com>
2021-09-07 10:40:31 +0300

    Add login page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,14 @@
 import { Route, Switch } from "react-router-dom";
 import Article from "./pages/article";
+import Authentication from "./pages/authentication";
 import GlobalFeed from "./pages/globalFeed";
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" exact component={GlobalFeed} />
+      <Route path="/login" component={Authentication} />
+      <Route path="/register" component={Authentication} />
       <Route path="/article/:slug" component={Article} />
     </Switch>
   );
